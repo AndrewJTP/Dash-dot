9b08575b45190b640ca914e0868d8a1caab590c2
Andrew <andrew.j.t.p@gmail.com>
2021-05-10 22:24:17 -0400

    clase tablero creado

---
+++ lib/tablero.rb
@@ -0,0 +1,16 @@
+class Tablero   
+    # método inicializar clase
+    def initialize(alto, largo)  
+        # atributos           
+        @alto = alto 
+        @largo=largo
+    end  
+   
+    def getAlto()
+        return @alto
+    end
+    def getLargo()
+        return @largo
+    end
+
+end  
--- spec/dashDot_spec.rb
+++ spec/dashDot_spec.rb
@@ -1,12 +1,21 @@
 require './lib/auto.rb'
+require './lib/tablero.rb'
+
 RSpec.describe "dash dot" do
-    it "deberia devolver el la orientacion del auto creado si le pasamos 'N' '1,2'" do
+    it "deberia devolver el la orientacion N del auto creado si le pasamos 'N' '1,2'" do
         expect(Auto.new('N',1,2).getOrientacion).to eq 'N' 
     end
-    it "deberia devolver la posicion X del auto creado si le pasamos 'N' 1,2" do
+    it "deberia devolver la posicion X 1 del auto creado si le pasamos 'N' 1,2" do
         expect(Auto.new('N',1,2).getPosicion_x).to eq 1 
     end
-    it "deberia devolver la posicion Y del auto creado si le pasamos 'N' 1,2" do
+    it "deberia devolver la posicion Y 2 del auto creado si le pasamos 'N' 1,2" do
         expect(Auto.new('N',1,2).getPosicion_y).to eq 2 
     end
+    it "deberia devolver la altura 3 del tablero si le pasamos 3,5" do
+        expect(Tablero.new(3,5).getAlto).to eq 3 
+    end
+    it "deberia devolver el largo 5 del tablero si le pasamos 3,5" do
+        expect(Tablero.new(3,5).getLargo).to eq 5
+    end    
+    
 end
